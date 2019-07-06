40ce1a7492965c7a17acfadb5895d1d2f41aa18d
Domenico Violi <dome.violi90@gmail.com>
2019-07-06 23:47:07 +0200

    hello world and first arithmetic operators

---
+++ chapter_01/arithmeticOperators.py
@@ -0,0 +1,20 @@
+#addition 
+print('40+2 =',40+2)
+
+#subtraction
+print('43-1 =',43-1)
+
+#multiplication
+print('6*7 =',6*7)
+
+#double division
+print('84/2 =',84/2)
+
+#integer division
+print('84//2 =',84//2)
+
+#exponentiation
+print('6**2 =',6**2)
+
+#XOR
+print('6^3 =',6^3)
