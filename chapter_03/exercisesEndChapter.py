b0b567cbba3a3396ade2145403b6bba1db80fb3a
Domenico Violi <dome.violi90@gmail.com>
2019-07-08 22:50:30 +0200

    add all exercise 3.2

--- chapter_03/exercisesEndChapter.py
+++ chapter_03/exercisesEndChapter.py
@@ -7,3 +7,47 @@ def giustif_destra(word):
     print(' '*repeatSpace+word)
 
 giustif_destra('I sleep all night and I work all day.')
+
+#3.2
+#.1 Scrivete questo esempio in uno script e provatelo.
+def do_twice(f):
+    f()
+    f()
+def print_spam():
+    print('spam') 
+do_twice(print_spam)
+
+#.2 Modificate do_twice in modo che accetti due argomenti,
+# un oggetto funzione e un valore, e che chiami la funzione
+# due volte passando il valore come argomento.
+
+def do_twice(f,value):
+    f(value)
+    f(value)
+
+def print_spam(valore):
+    print(valore)
+
+#.3 Copiate nel vostro script la definizione di print_twice che abbiamo
+# visto nel corso di questo capitolo.
+print('')
+do_twice(print_spam,'hello')
+def do_twice(bruce):
+    print(bruce)
+    print(bruce)
+# Usate la versione modificata di do_twice per chiamare
+# do_twice per due volte, passando 'spam' come argomento.
+
+print('')
+do_twice(print_twice,'spam')
+
+# Definite una nuova funzione di nome fai_quattro che richieda un
+# oggetto funzione e un valore e chiami la funzione per 4 volte,
+# passando il valore come argomento. Dovrebbero esserci solo due
+# istruzioni nel corpo di questa funzione, non quattro.
+
+def do_four(f,value):
+    do_twice(f,value)
+    do_twice(f,value)
+print('')
+do_four(print_twice,'spam')
